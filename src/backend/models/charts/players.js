ac0fc7985dac41eca33527a0212288a8bf68c8df
Quinten Peels <quinten@peels.me>
2020-10-09 13:12:29 +0200

    [Feature] Updated packages, fixed monthly chart sql query

--- src/backend/models/charts/players.js
+++ src/backend/models/charts/players.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     async getPlayersLastMonth() {
         return queryAndCache(
-            'SELECT ROUND(AVG(`players`), 0) as players, DATE_FORMAT(date,\'%m-%Y\') as month_format, MONTHNAME(date) as month_name FROM players WHERE server_online = TRUE AND date > UTC_TIMESTAMP() - INTERVAL 1 MONTH GROUP BY month_format, month_name LIMIT 30',
+            'SELECT ROUND(AVG(`players`), 0) as players, DATE_FORMAT(date,\'%m-%Y\') as month_format, MONTHNAME(date) as month_name FROM players WHERE server_online = TRUE AND date > UTC_TIMESTAMP() - INTERVAL 12 MONTH GROUP BY month_format, month_name LIMIT 30',
             "players_past_month",
             60,
             false
