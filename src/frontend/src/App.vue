bbc8019fd9275be9086c46d5391ba2f0194c8dea
Quinten Peels <quinten@peels.me>
2021-04-13 18:10:20 +0200

    [Fix] Revert mysql to 5.7 duo compatability

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -39,9 +39,6 @@
 <script>
 import axios from "axios";
 
-axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
-axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
-
 export default {
   name: "App",
   components: {},
