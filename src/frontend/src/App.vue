688ab0ebc0e6a425282ea023edd6e91b97df33c1
Quinten Peels <quinten@peels.me>
2021-04-13 17:41:28 +0200

    [Refactor] Axios cors

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -39,6 +39,9 @@
 <script>
 import axios from "axios";
 
+axios.defaults.headers.post["Content-Type"] = "application/json;charset=utf-8";
+axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
 export default {
   name: "App",
   components: {},
